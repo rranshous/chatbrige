147f63f5029288db79a976706b934cd5a362f591
Robby Ranshous <rranshous@gmail.com>
2016-06-12 22:49:16 -0400

    simple admin ui, still missing logs

--- subscription_manager.rb
+++ subscription_manager.rb
@@ -13,6 +13,7 @@ puts "docker_image_name: #{DOCKER_IMAGE_NAME}"
 
 def log msg
   puts msg
+  msg
 end
 
 module Subscription
@@ -25,13 +26,38 @@ module Subscription
 end
 
 class BridgeProcess
-  def self.for_subscription subscription
+  def self.from_subscription subscription
     new subscription.api_key,
         subscription.room,
         subscription.sender,
         subscription.target,
         subscription.poll_delay
   end
+
+  def self.from_config config
+    new config['api_key'],
+        config['room'],
+        config['sender'],
+        config['target'],
+        config['poll_delay']
+  end
+
+  def self.all_active
+    Docker::Container.all.map do |container|
+      container_labels = container.json["Config"]["Labels"]
+      if container_labels['app.name'] == "chatbridge_sender"
+        from_config Hash[container_labels.to_a.map do |(k,v)|
+          if k.start_with? 'bridge_manager.config.'
+            [k.gsub('bridge_manager.config.',''), v]
+          else
+            nil
+          end
+        end.compact]
+      end
+    end
+  end
+
+  attr_reader :api_key, :room, :sender, :target, :poll_delay
 
   def initialize api_key, room, sender, target, poll_delay
     @api_key, @room, @sender, @target = api_key, room, sender, target
@@ -59,7 +85,8 @@ class BridgeProcess
 
   def find_container
     Docker::Container.all.each do |container|
-      if container.json["Config"]["Labels"] == labels
+      container_labels = container.json["Config"]["Labels"]
+      if container_labels == labels
         return container
       end
     end
@@ -101,27 +128,51 @@ class BridgeProcess
 
   def labels
     {
-      'hipchat_room' => @room,
-      'hipchat_api_key' => @api_key,
-      'hipchat_sender' => @sender,
-      'message_target' => @target
+      'app.name' => 'chatbridge_sender',
+      'bridge_manager.config.room' => @room.to_s,
+      'bridge_manager.config.api_key' => @api_key.to_s,
+      'bridge_manager.config.sender' => @sender.to_s,
+      'bridge_manager.config.target' => @target.to_s,
+      'bridge_manager.config.poll_delay' => @poll_delay.to_s,
     }
   end
 end
 
-post "/add_subscription" do
+post "/remove_subscription" do
   data_in = JSON.parse(request.body.read)
+  log "data_in: #{data_in}"
   subscription = Subscription.from_options(data_in)
   log "subscription: #{subscription}"
-  subscription.room or
-    (log "failed req: missing room" and halt 400, "missing room")
-  subscription.sender or
-    (log "failed req: missing sender" and halt 400, "missing sender")
-  subscription.api_key or
-    (log "failed req: missing api_key" and halt 400, "missing api_key")
-  subscription.target or
-    (log "failed req: missing target" and halt 400, "missing target")
-  bridge = BridgeProcess.for_subscription subscription
+  bridge = BridgeProcess.from_subscription subscription
+  bridge.stop
+  forward_url = "/check_subscription?#{URI.encode_www_form(subscription.to_h)}"
+  redirect forward_url, 303
+end
+
+post "/remove_subscription/:bridge" do |bridge_config_encoded|
+  data_in = JSON.parse(Base64.urlsafe_decode64(bridge_config_encoded))
+  log "data_in: #{data_in}"
+  subscription = Subscription.from_options(data_in)
+  log "subscription: #{subscription}"
+  bridge = BridgeProcess.from_subscription subscription
+  bridge.stop
+  forward_url = "/check_subscription?#{URI.encode_www_form(subscription.to_h)}"
+  redirect forward_url, 303
+end
+
+post "/add_subscription/form_encoded" do
+  puts "params: #{params}"
+  subscription = Subscription.from_options(params)
+  log "subscription: #{subscription}"
+  subscription.room.empty? and
+    log "failed req: missing room" and halt 400, "missing room"
+  subscription.sender.empty? and
+    log "failed req: missing sender" and halt 400, "missing sender"
+  subscription.api_key.empty? and
+    log "failed req: missing api_key" and halt 400, "missing api_key"
+  subscription.target.empty? and
+    log "failed req: missing target" and halt 400, "missing target"
+  bridge = BridgeProcess.from_subscription subscription
   started = bridge.start
   forward_url = "/check_subscription?#{URI.encode_www_form(subscription.to_h)}"
   if started
@@ -134,19 +185,34 @@ end
 get "/check_subscription" do
   subscription = Subscription.from_options(params)
   log "subscription: #{subscription}"
-  bridge = BridgeProcess.for_subscription subscription
+  bridge = BridgeProcess.from_subscription subscription
   running = bridge.is_running?
   log "running?: #{running}"
   content_type :json
   { running: running }.to_json
 end
 
-post "/remove_subscription" do
-  data_in = JSON.parse(request.body.read)
-  subscription = Subscription.from_options(data_in)
-  log "subscription: #{subscription}"
-  bridge = BridgeProcess.for_subscription subscription
-  bridge.stop
-  forward_url = "/check_subscription?#{URI.encode_www_form(subscription.to_h)}"
-  redirect forward_url, 303
+get "/" do
+  bridges = BridgeProcess.all_active
+  @data = {
+    bridges: bridges.map{ |bridge|
+      OpenStruct.new({
+        api_key_snippet: bridge.api_key[-5..-1],
+        api_key: bridge.api_key,
+        room: bridge.room,
+        sender: bridge.sender,
+        target: bridge.target,
+        poll_delay: bridge.poll_delay,
+        encoded_config: Base64.urlsafe_encode64({
+          api_key: bridge.api_key,
+          room: bridge.room,
+          sender: bridge.sender,
+          target: bridge.target,
+          poll_delay: bridge.poll_delay
+        }.to_json)
+      })
+    }
+  }
+  erb :'index.html'
 end
+
